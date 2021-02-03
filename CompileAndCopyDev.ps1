8391cb8226a2632ac380209145e1e5719b08c7b2
Igor Kiselyov <igorkiselyov74@live.com>
2021-02-03 22:43:44 +0300

    compile and move to \missions, startis only (testing purposes)

---
+++ CompileAndCopyDev.ps1
@@ -0,0 +1,25 @@
+$root = $PSScriptRoot;
+
+$buildFolder = $env:userprofile + "\Documents\Arma 3 - Other Profiles\Socrates\missions";
+
+$devFolderItem = Get-ChildItem -Path ".\Map-Templates\Antistasi-Stratis.Stratis" | Select-Object -First 1;
+$devFolder = $devFolderItem.parent;
+
+$mainDataPath = Join-Path $root 'A3-Antistasi';
+$stringTablePath = Join-Path $root 'A3-Antistasi\Stringtable.xml';
+
+$stringTable = New-Object -TypeName XML;
+$stringTable.Load($stringTablePath);
+
+$versionId = $stringTable.Project.Package.Container | Where-Object { $_.name -eq "credits_generic" } | ForEach-Object {$_.Key} | Where-Object {$_.ID -eq "STR_antistasi_plus_credits_generic_version_text"} | ForEach {$_.Original};
+$formattedVersionId = $versionId.Split("\.") -join "-";
+
+$folderName = $devFolder.Name;
+$pair = $folderName.Split("\.");
+$missionFolderName = $pair[0] + "-" + $formattedVersionId + "-" + "Plus" + "-" + "dev" + "." + $pair[1]; 
+$destinationPath = $(Join-Path $buildFolder $missionFolderName);
+
+Remove-Item $destinationPath -Recurse -ErrorAction Ignore;
+
+Copy-Item -Path $mainDataPath -Destination $destinationPath -Recurse;
+Copy-Item -Path $(Join-Path $devFolder.FullName "*") -Destination $destinationPath -Recurse -Force;
