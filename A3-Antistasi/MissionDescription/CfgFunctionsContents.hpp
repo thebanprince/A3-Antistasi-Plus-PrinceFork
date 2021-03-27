9efd5f13bf4382c7a58dd8cb30e1c83f8190ff39
Igor Kiselyov <igorkiselyov74@live.com>
2021-03-27 17:38:29 +0300

    new configs

---
+++ A3-Antistasi/MissionDescription/CfgFunctionsContents.hpp
@@ -0,0 +1,11 @@
+// In map template description.ext use:
+// #include "MissionDescription\CfgFunctionsContents.hpp"
+
+#define HALs_DEF_FUNCTIONS
+		#include "..\HALs\Addons\main\config.hpp"
+#undef HALs_DEF_FUNCTIONS
+
+#include "..\functions.hpp"
+#include "..\JeroenArsenal\functions.hpp"
+//#include "..\Collections\functions.hpp"
+#include "..\SCRT\functions.hpp"
