f3a7db6325b313e2fc2426456ba3fbed4d7290da
Igor Kiselyov <igorkiselyov74@live.com>
2021-07-14 18:52:14 +0300

    plus version in mission name

--- PrepareMissionForPackingNoSqfc.ps1
+++ PrepareMissionForPackingNoSqfc.ps1
@@ -13,12 +13,14 @@ $stringTable = New-Object -TypeName XML;
 $stringTable.Load($stringTablePath);
 
 $versionId = $stringTable.Project.Package.Container | Where-Object { $_.name -eq "credits_generic" } | ForEach-Object {$_.Key} | Where-Object {$_.ID -eq "STR_antistasi_credits_generic_version_text"} | ForEach {$_.Original};
+$plusVersionId = $stringTable.Project.Package.Container | Where-Object { $_.name -eq "credits_generic" } | ForEach-Object {$_.Key} | Where-Object {$_.ID -eq "STR_antistasi_plus_credits_generic_version_text"} | ForEach {$_.Original};
 $formattedVersionId = $versionId.Split("\.") -join "-";
+$formattedPlusVersionId = $plusVersionId.Split("\.") -join "-";
 
 ForEach ($templateFolder in $missionTemplateFolders) {
 	$folderName = $templateFolder.Name;
 	$pair = $folderName.Split("\.");
-	$missionFolderName = $pair[0] + "-" + $formattedVersionId + "-" + "Plus" + "." + $pair[1]; 
+	$missionFolderName = $pair[0] + "-" + $formattedVersionId + "-" + "Plus" + "-" + $formattedPlusVersionId + "." + $pair[1]; 
 	$destinationPath = $(Join-Path $folderForPreparedMissions.FullName $missionFolderName);
 	Copy-Item -Path $mainDataPath -Destination $destinationPath -Recurse;
 	Copy-Item -Path $(Join-Path $templateFolder.FullName "*") -Destination $destinationPath -Recurse -Force;
